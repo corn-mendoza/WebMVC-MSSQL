223c90b92a147515815e9423767f0c36171d46c3
Corn Mendoza <cmendoza@pivotal.io>
2017-06-23 13:21:39 -0500

    Added Database Initialization

--- WebMVC-MSSQL/Startup.cs
+++ WebMVC-MSSQL/Startup.cs
@@ -51,6 +51,46 @@ namespace WebMVC_MSSQL
                 app.UseExceptionHandler("/Home/Error");
             }
 
+
+            // Perform some database initialisation.
+
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+
+            {
+
+                var dbContext = serviceScope.ServiceProvider.GetService<ReleaseContext>();
+
+                // Database.Migrate() will perform a migration of the database. This will ensure that the target database
+                // is in sync with current context model snapshot found in the Migrations folder.
+                // The alternative is to use EnsureCreated(). This will create a database and tables, if not existent on server.
+                // However, consequently this skips migration altogether. Future migrations will not be possible
+                // and one must issue EnsureDeleted() each time at the end to pull down the database.  
+                // Therefore use EnsureCreated()/EnsureDeleted() for testing/development purposes only.
+                // Note: On CF, it appears that EnsureDeleted may not work. For MySql dbs, it tries to 
+                // access the 'mysql' database and fails because our random bound user (via cf bind-service) cannot 
+                // this internal database.
+
+                // For clarity and compatibility, we'll stick to Database.Migrate()
+                // We do migrate here because potentially, one would need to initialise
+                // a database on CF that may only be internal to CF or do further migrations in future. 
+                // This will require a snapshot to be created first via dotnet ef migrations add <somename>.  
+                // If access to CF database is not possible, point to a local database first.
+
+                dbContext.Database.Migrate();
+
+                //if (env.IsDevelopment())
+
+                //{
+
+                //    // Ensure that we have some dummy data when we're performing development.
+
+                //    dbContext.EnsureSeedData();
+
+                //}
+
+            }
+
+
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
